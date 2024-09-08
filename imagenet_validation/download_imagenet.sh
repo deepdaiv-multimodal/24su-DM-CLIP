28130aa41964879b72fe958ce5ad2cb5448b18c0
Taewan Cho <taewan2002@naver.com>
2024-09-08 14:04:04 +0900

    Update download_imagenet.sh

--- imagenet_validation/download_imagenet.sh
+++ imagenet_validation/download_imagenet.sh
@@ -1,1 +1,3 @@
-imagenet_validation/download_imagenet.sh
+wget https://image-net.org/data/ILSVRC/2012/ILSVRC2012_img_val.tar --no-check-certificate
+mkdir val && mv ILSVRC2012_img_val.tar val/ && cd val && tar -xvf ILSVRC2012_img_val.tar
+wget -qO- https://raw.githubusercontent.com/soumith/imagenetloader.torch/master/valprep.sh | bash
