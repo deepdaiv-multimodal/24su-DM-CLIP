95c149775b8e2eb56504f5f73ba7a81f3949c9a5
harim061 <doris5093@duksung.ac.kr>
2024-09-26 21:10:25 +0900

    add : benchmark

---
+++ clip_benchmark.sh
@@ -0,0 +1,8 @@
+clip_benchmark eval \
+    --model_type "mobile_clip" \
+    --model "mobileclip_s2" \
+    --dataset "webdatasets.txt" \
+    --dataset_root "https://huggingface.co/datasets/clip-benchmark/wds_{dataset_cleaned}/tree/main" \
+    --output "/root/code/harim/DeepDaiv/M4-CLIP/benchmark/output/benchmark_mobileclip_{dataset}_{pretrained}_{model}_{language}_{task}.json"
+
+# model "mobileclip_s1"
